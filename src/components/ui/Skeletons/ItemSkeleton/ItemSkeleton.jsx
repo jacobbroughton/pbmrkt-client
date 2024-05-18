8fe34717daae852470304dac12905dab91e4e28f
Jacob Broughton <jlbroughton88@gmail.com>
2024-05-18 07:08:00 -0400

    added listing skeletons

--- src/components/ui/Skeletons/ItemSkeleton/ItemSkeleton.jsx
+++ src/components/ui/Skeletons/ItemSkeleton/ItemSkeleton.jsx
@@ -1,6 +1,12 @@
+import "./ItemSkeleton.css";
+
 const ItemSkeleton = () => {
   return (
-    <div className='item-skeleton skeleton'>&nbsp;</div>
-  )
-}
-export default ItemSkeleton
+    <div className="item-skeleton-container">
+      <div className="image-skeleton skeleton">&nbsp;</div>
+      <div className="words-skeleton skeleton">&nbsp;</div>
+      <div className="words-skeleton short  skeleton">&nbsp;</div>
+    </div>
+  );
+};
+export default ItemSkeleton;

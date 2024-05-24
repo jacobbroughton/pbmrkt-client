b655df5c77f93b24ac3ccaded6a5a4e9e88e6d2b
Jacob Broughton <jlbroughton88@gmail.com>
2024-05-24 16:28:29 -0400

    added profile picture and username item listing skeletons

--- src/components/ui/Skeletons/ItemSkeleton/ItemSkeleton.jsx
+++ src/components/ui/Skeletons/ItemSkeleton/ItemSkeleton.jsx
@@ -6,6 +6,10 @@ const ItemSkeleton = () => {
       <div className="image-skeleton skeleton">&nbsp;</div>
       <div className="words-skeleton short skeleton">&nbsp;</div>
       <div className="words-skeleton skeleton">&nbsp;</div>
+      <div className="profile-picture-and-name-skeletons">
+        <div className="profile-picture skeleton"></div>
+        <div className="words-skeleton skeleton"></div>
+      </div>
     </div>
   );
 };

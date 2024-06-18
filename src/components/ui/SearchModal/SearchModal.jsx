1c973a18727d0495e287efe9abe87fc015037f10
Jacob Broughton <jlbroughton88@gmail.com>
2024-06-18 12:49:33 -0400

    updated search

--- src/components/ui/SearchModal/SearchModal.jsx
+++ src/components/ui/SearchModal/SearchModal.jsx
@@ -4,11 +4,17 @@ import XIcon from "../Icons/XIcon";
 import { toggleModal } from "../../../redux/modals";
 import "./SearchModal.css";
 import SearchIcon from "../Icons/SearchIcon";
-import { useRef, useState } from "react";
-import { setDraftSearchValue } from "../../../redux/search";
+import { useEffect, useRef, useState } from "react";
+import { setDraftSearchValue, setSavedSearchValue } from "../../../redux/search";
+import { setFlag } from "../../../redux/flags";
+import { setFiltersUpdated } from "../../../redux/filters";
+import { Link, useNavigate } from "react-router-dom";
+import { supabase } from "../../../utils/supabase";
+import { getTimeAgo } from "../../../utils/usefulFunctions";
 
 const SearchModal = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const searchRef = useRef();
   const search = useSelector((state) => state.search);
   const [searchResults, setSearchResults] = useState({
@@ -19,7 +25,80 @@ const SearchModal = () => {
     { id: 0, label: "Listings", resultsCount: 10, toggled: true },
     { id: 1, label: "Users", resultsCount: 2, toggled: false },
   ]);
+  const [error, setError] = useState();
   const [resultsLoading, setResultsLoading] = useState(false);
+  const [searchIsInitial, setSearchIsInitial] = useState(true);
+
+  function handleSearcOldh() {
+    console.log("handleSearch", search);
+    dispatch(setFlag({ key: "searchedListingsNeedUpdate", value: true }));
+    dispatch(setFiltersUpdated(false));
+    dispatch(toggleModal({ key: "searchModal", value: false }));
+  }
+
+  function handleSearch(e) {
+    e.preventDefault();
+    try {
+      if (search.draftSearchValue === search.savedSearchValue) return;
+      dispatch(setSavedSearchValue(search.draftSearchValue));
+      dispatch(setFlag({ key: "searchedListingsNeedUpdate", value: true }));
+      dispatch(toggleModal({ key: "searchModal", value: false }));
+      setSearchIsInitial(false);
+      searchRef.current?.blur();
+    } catch (error) {
+      console.error(error);
+      setError(error.message.toString());
+    }
+  }
+
+  async function handleOnInputSearch(searchValue) {
+    try {
+      setResultsLoading(true);
+
+      let { data, error } = await supabase.rpc("get_search_result_listing_previews", {
+        p_search_value: searchValue,
+      });
+
+      if (error) throw error.message;
+
+      data = data.map((img) => {
+        const { data, error } = supabase.storage
+          .from("item_images")
+          .getPublicUrl(img.path);
+
+        if (error) throw error.message;
+
+        return {
+          ...img,
+          image_url: data.publicUrl,
+        };
+      });
+
+      setSearchResults({
+        ...searchResults,
+        listings: data,
+      });
+      setSearchIsInitial(false);
+    } catch (error) {
+      console.log(error);
+      setError(error.toString());
+    }
+
+    setResultsLoading(false);
+  }
+
+  useEffect(() => {
+    const debounceFn = setTimeout(() => {
+      if (search.draftSearchValue == "" && searchIsInitial) return;
+      dispatch(setSavedSearchValue(search.draftSearchValue));
+      handleOnInputSearch(search.draftSearchValue);
+    }, 500);
+
+    return () => clearTimeout(debounceFn);
+  }, [search.draftSearchValue]);
+
+  const selectedSearchType = searchTypes.find((type) => type.toggled);
+  const resultsForView = searchResults[selectedSearchType?.label?.toLowerCase()];
 
   return (
     <>
@@ -30,18 +109,23 @@ const SearchModal = () => {
             <XIcon /> Close
           </button>
         </div> */}
-        <p><i>This feature does not work yet</i></p>
+        {error && <p className="small-text error-text">{error.toString()}</p>}
+        <p>
+          <i>This feature is under development</i>
+        </p>
         <div className="search-input-container">
           <SearchIcon />
-          <input
-            placeholder="Search for anything (ex. Planet Eclipse, LTR, Sandana)"
-            value={search.draftSearchValue}
-            onChange={(e) => dispatch(setDraftSearchValue(e.target.value))}
-            ref={searchRef}
-          />
-          <button onClick={() => null} className="search-apply-button">
-            Search
-          </button>
+          <form onSubmit={handleSearch}>
+            <input
+              placeholder="Search for anything (ex. Planet Eclipse, LTR, Sandana)"
+              value={search.draftSearchValue}
+              onChange={(e) => dispatch(setDraftSearchValue(e.target.value))}
+              ref={searchRef}
+            />
+            <button type="submit" className="search-apply-button">
+              Search
+            </button>
+          </form>
         </div>
         <div className="search-results-container">
           <div className="search-types-list">
@@ -50,12 +134,6 @@ const SearchModal = () => {
                 id={searchType.label}
                 className={`${searchType.toggled ? "toggled" : ""}`}
                 onClick={() => {
-                  if (
-                    searchType.id !==
-                    searchTypes.find((innerSearchType) => innerSearchType.toggled)?.id
-                  ) {
-                    setResultsLoading(true);
-                  }
 
                   setSearchTypes(
                     searchTypes.map((innerSearchType) => ({
@@ -64,10 +142,16 @@ const SearchModal = () => {
                     }))
                   );
 
-                  setTimeout(() => {
-                    setResultsLoading(false);
-                    // * Api call would be here instead of the settimeout
-                  }, 1000);
+                  if (
+                    searchType.id !== selectedSearchType?.id &&
+                    search.savedSearchValue !== ""
+                  ) {
+                    setResultsLoading(true);
+                    setTimeout(() => {
+                      setResultsLoading(false);
+                      // * Api call would be here instead of the settimeout
+                    }, 1000);
+                  }
                 }}
               >
                 {searchType.label}
@@ -79,18 +163,55 @@ const SearchModal = () => {
               <div className="results-loading">
                 <p>Results loading...</p>
               </div>
-            ) : [].length == 0 ? (
+            ) : search.savedSearchValue == "" ? (
+              <p className="type-something-prompt">Type something to get started</p>
+            ) : resultsForView.length == 0 ? (
               <div className="no-search-results">
-                <p>No results found for "asdfdsfs"</p>
+                <p>
+                  No results found for "{search.savedSearchValue}" in{" "}
+                  <strong>{selectedSearchType.label}</strong>
+                </p>
+                {console.log(selectedSearchType)}
               </div>
+            ) : selectedSearchType.label == "Listings" ? (
+              <ul className="listings">
+                {searchResults.listings.map((listing) => (
+                  <li>
+                    <Link
+                      onClick={() =>
+                        dispatch(toggleModal({ key: "searchModal", value: false }))
+                      }
+                      to={`${listing.id}`}
+                    >
+                      <div className="image-container">
+                        <img src={listing.image_url} />
+                      </div>
+                      {console.log(listing)}
+                      <div className="listing-info">
+                        <p className="what-is-this">{listing.what_is_this}</p>
+                        <p className="username">
+                          Listed by {listing.created_by_username}{" "}
+                          {getTimeAgo(new Date(listing.created_dttm))}
+                        </p>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : selectedSearchType.label == "Users" ? (
+              <div className="user-results">Search users found</div>
             ) : (
-              <div>Search results found</div>
+              false
             )}
           </div>
         </div>
       </div>
       <ModalOverlay
-        onClick={() => dispatch(toggleModal({ key: "searchModal", value: false }))}
+        onClick={() => {
+          dispatch(toggleModal({ key: "searchModal", value: false }));
+          dispatch(setDraftSearchValue(""));
+          dispatch(setSavedSearchValue(""));
+        }}
       />
     </>
   );
